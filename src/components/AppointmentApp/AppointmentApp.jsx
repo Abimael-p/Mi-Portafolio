c9abc8b786ff52564f81cd3bd654edcefab070c5
Abimael Junior Pacheco <122593014+Abimael-p@users.noreply.github.com>
2024-02-01 19:19:43 -0500

    update data /24

---
+++ src/components/AppointmentApp/AppointmentApp.jsx
@@ -0,0 +1,29 @@
+import "./AppointmentApp";
+
+const AppointmentApp = () => {
+    return (
+      <div className="container-proyect-appointmentApp">
+        <img
+          src="https://i.ibb.co/kq5jZKF/Appointment-App.png"
+          alt="AppointmentApp"
+        />
+        <div className="container-content-appointmentApp">
+          <h3>Appointment App</h3>
+          <p>
+          "En el apasionante mundo de la simulación de 'nocountry', nuestro 
+          equipo de 6 personas ha creado una aplicación innovadora para gestionar 
+          citas con especialistas. Nuestra plataforma permite a los clientes programar 
+          fácilmente citas con los especialistas disponibles, brindando una experiencia 
+          de usuario fluida y eficiente.
+          </p>
+          <div className="btn-appointmentApp">
+            <a href="https://appointment-app-chi.vercel.app/">
+              <button>Visitar</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AppointmentApp;
