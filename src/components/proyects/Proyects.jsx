f7bbdfa8aad728c3913c7c897287251d6db28c2d
Abimael Junior Pacheco <122593014+Abimael-p@users.noreply.github.com>
2024-04-23 05:14:26 +0000

    upd_comprojec

--- src/components/proyects/Proyects.jsx
+++ src/components/proyects/Proyects.jsx
@@ -4,19 +4,28 @@ import RickAndMorty from "../RickAndMorty/RickAndMorty";
 import SkyWatch from "../SkyWatch/SkyWatch";
 import UsersAdmin from "../UsersAdmin/UsersAdmin";
 import AppointmentApp from "../AppointmentApp/AppointmentApp";
+import { useState } from "react";
 import "./Proyects.css";
 
 const Proyects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const projects = [<Ecommerce />, <FortuneCookies />, <RickAndMorty />, <SkyWatch />, <UsersAdmin />, <AppointmentApp />];
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <section className="container-proyects">
       <h2>Mis Front-End</h2>
       <div className="card-proyect">
-        <SkyWatch />
-        <RickAndMorty />
-        <Ecommerce />
-        <FortuneCookies />
-        <UsersAdmin />
-        <AppointmentApp />
+        <i className="bx bx-chevrons-left" onClick={handlePrev}></i>
+        {projects[currentIndex]}
+        <i className="bx bx-chevrons-right" onClick={handleNext}></i>
       </div>
     </section>
   );
